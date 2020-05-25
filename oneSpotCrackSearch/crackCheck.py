acf7685f18d6548a6904cacf9ba6b3aedd8a366c
vijay <vijaybalajitheboss@gmail.com>
2020-05-25 22:07:04 +0530

    Completed crackCheck.py

--- oneSpotCrackSearch/crackCheck.py
+++ oneSpotCrackSearch/crackCheck.py
@@ -1,16 +1,27 @@
 import requests
 import json
 import urllib
-
-url = "https://api.crackwatch.com/api/games?&is_cracked=true&page=1"
+from time import sleep
+import sys
 
 def Crack_checker(link,title):
     url = urllib.request.urlopen(link)
     data = json.loads(url.read())
     strdata = str(data)
-    if (strdata.find(title) != -1):
-        print("Game is cracked")
+    if(strdata!="[]"):
+        if (strdata.find(title) != -1):
+            return True
+        else:
+            return False
     else:
         print("Game is not cracked")
-    
-Crack_checker(url,"Spirit of the North")
+        sleep(20)
+        sys.exit(0)
+
+game = input(str("Enter the name of the game you want to check for : "))
+for i in range(600):
+    i = str(i)    
+    url = ("https://api.crackwatch.com/api/games?&is_cracked=true&page="+i)
+    if(Crack_checker(url,game)):
+        print("Game is Cracked")
+        break
