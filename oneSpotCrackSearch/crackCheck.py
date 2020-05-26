24bef6f4c57c60460058028e47dc2cbe42082c40
dat-adi <naruita201@gmail.com>
2020-05-26 10:11:30 +0530

    Optimized imports

--- oneSpotCrackSearch/crackCheck.py
+++ oneSpotCrackSearch/crackCheck.py
@@ -1,32 +1,35 @@
-import requests
-import json
-import urllib
-import sys
-import requests_cache
+from json import loads
+from urllib.request import urlopen
+from sys import exit as Exit
+from requests_cache import install_cache
 
 requests_cache.install_cache('cache')
 def Crack_checker(link,title):
     try:
-        url = urllib.request.urlopen(link)
-    except:
-        print("CONNECTION ERROR : 500 INTERNAL SERVER ERROR")
-        input("Enter any key to exit ")
-        sys.exit(0)
-    data = json.loads(url.read())
-    strdata = str(data)
-    if(strdata!="[]"):
-        if (strdata.find(title) != -1):
-            return True
+        url = urlopen(link)
+        data = loads(url.read())
+        strdata = str(data)
+        if(strdata!="[]"):
+            if (strdata.find(title) != -1):
+                return True
+            else:
+                return False
         else:
-            return False
-    else:
-        print("Game is not cracked")
-        input("Enter any key to exit ")
-        sys.exit(0)
+            print("Game is not cracked")
+            input("Enter any key to exit ")
+            Exit(0)
+    except:
+        print("Connection error.")
+        input("Press any key to exit")
+        Exit(0)
 
 game = input(str("Enter the name of the game you want to check for : "))
-for i in range(600):
-    i = str(i)    
+number_of_pages = int(input("How many pages do you want to search?\n"))
+for i in range(number_of_pages):
+    print("searching...")
+    if(i >= 50 and i%50):
+        print("In page {}, will continue searching...".format(i))
+    i = str(i)
     url = ("https://api.crackwatch.com/api/games?&is_cracked=true&page="+i)
     if(Crack_checker(url,game)):
         print("Game is Cracked")
