e16d2200731d9308f436010ef9e7e11179afcf83
dat-adi <naruita201@gmail.com>
2020-06-14 13:08:25 +0530

    Inserts filenames into csv

--- dataset-maker/test_dataset_maker.py
+++ dataset-maker/test_dataset_maker.py
@@ -1,9 +1,12 @@
-import csv
 from os import listdir
+from csv import writer
 
 
 def file_indexer(dir_path):
     print(listdir(dir_path))
+    with open('temp.csv', 'w', newline='') as f:
+        wtr = writer(f)
+        wtr.writerow(listdir(dir_path))
 
 
 if __name__ == "__main__":
