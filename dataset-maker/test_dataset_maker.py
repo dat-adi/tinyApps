b55a36fe2a59ec149b512189dcf717d217653de4
dat-adi <naruita201@gmail.com>
2020-06-15 11:35:04 +0530

    Updated extracted properties

--- dataset-maker/test_dataset_maker.py
+++ dataset-maker/test_dataset_maker.py
@@ -4,13 +4,19 @@ from csv import writer, DictWriter
 
 
 def file_indexer(dir_path):
-    print(listdir(dir_path))
     with open('temp.csv', 'w', newline='') as f:
         fieldnames=["File Name", "Extension", "Size"]
         wtr = DictWriter(f, fieldnames=fieldnames)
         for one_file in listdir(dir_path):
             file_name = dir_path + '\\' + one_file
-            wtr.writerow({"File Name" : one_file, "Extension" : splitext(file_name)[1], "Size" : getsize(file_name)})
+            wtr.writerow(
+                {
+                    "File Name" : one_file[:one_file.rfind('.')],
+                    "Extension" : splitext(file_name)[1],
+                    "Size" : getsize(file_name),
+                    "Path" : file_name
+                }
+            )
 
 
 if __name__ == "__main__":
