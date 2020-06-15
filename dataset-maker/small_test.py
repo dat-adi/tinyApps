b607772504c88ff7768ec1064272104b12613018
dat-adi <naruita201@gmail.com>
2020-06-15 12:20:52 +0530

    Added in file type recognition

--- dataset-maker/small_test.py
+++ dataset-maker/small_test.py
@@ -1,7 +1,12 @@
 import os
 
-temp_file = "D:\\tinyApps\\oneSpotCrackSearch"
+temp_file = "D:\\tinyApps"
 print(os.listdir(temp_file))
 for one_file in os.listdir(temp_file):
     file_name = temp_file + '\\' + one_file
-    print(one_file, os.path.getsize(file_name), os.path.splitext(file_name)[1])
+    if os.path.getsize(file_name) != 0:
+        print(
+            one_file,
+            os.path.getsize(file_name),
+            os.path.splitext(file_name)[1]
+        )
--- dataset-maker/test_dataset_maker.py
+++ dataset-maker/test_dataset_maker.py
@@ -5,26 +5,50 @@ from csv import writer, DictWriter
 
 def file_indexer(dir_path):
     with open('temp.csv', 'w', newline='') as f:
-        fieldnames=["File Name", "Extension", "Size", "Path"]
+        fieldnames=["File Name", "Extension", "File Type", "Size", "Path"]
         wtr = DictWriter(f, fieldnames=fieldnames)
         wtr.writerow(
             {
                 "File Name": "File Name",
                 "Extension": "Extension",
+                "File Type": "File Type",
                 "Size": "Size",
                 "Path": "Path"
             }
         )
+
+        image_extension_list = ['.jpg', '.png', '.svg']
+        text_extension_list = ['.txt', '.md', '.html']
+        style_extension_list = ['.css', '.scss']
+        code_extension_list = ['.py', '.java', '.c', '.cpp', '.js']
+
         for one_file in listdir(dir_path):
+
+            directories_in_folder = []
+
             file_name = dir_path + '\\' + one_file
-            wtr.writerow(
-                {
-                    "File Name" : one_file[:one_file.rfind('.')],
-                    "Extension" : splitext(file_name)[1],
-                    "Size" : getsize(file_name),
-                    "Path" : file_name
-                }
-            )
+            if splitext(file_name)[1] in image_extension_list:
+                file_type = 'image'
+            elif splitext(file_name)[1] in text_extension_list:
+                file_type = 'text'
+            elif splitext(file_name)[1] in style_extension_list:
+                file_type = 'style'
+            elif splitext(file_name)[1] in code_extension_list:
+                file_type = 'programming'
+
+            if getsize(file_name) != 0:
+                wtr.writerow(
+                    {
+                        "File Name" : one_file[:one_file.rfind('.')],
+                        "Extension" : splitext(file_name)[1],
+                        "File Type" : file_type,
+                        "Size" : getsize(file_name),
+                        "Path" : file_name
+                    }
+                )
+            else:
+                directories_in_folder.append(one_file)
+    print("Directories present in the folder are : ", directories_in_folder)
 
 
 if __name__ == "__main__":
