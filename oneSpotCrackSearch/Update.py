35e80cb8836666207d911f2d0cb56549c179f07c
vijay <vijaybalajitheboss@gmail.com>
2020-06-16 14:07:27 +0530

    Displays last updated date and time

--- oneSpotCrackSearch/Update.py
+++ oneSpotCrackSearch/Update.py
@@ -9,18 +9,30 @@ def update(link):
     url = urlopen(link)
     data = loads(url.read())
     strdata = str(data)
-    path = "oneSpotCrackSearch/assets/Data.txt"
+    path = "assets/Data.txt"
     with open(path,"a",encoding="utf-8") as f:
         f.write(strdata)
     f.close()
 
+date = open("assets/LastUpdatedOn.txt","r")
+DateAndTime = date.read()
+if(len(DateAndTime)!=0):
+    print("Last Updated On " + DateAndTime)
+date.close()
+
 print("Update Process Started...\nThis might take a while. We will notify you once it is done.")
-for i in range(500):
+for i in range(1):
     i = str(i)
     url = ("https://api.crackwatch.com/api/games?&is_cracked=true&page="+i)
     update(url)
     print("Page " + i + " has been added")
 
 print("Data has been successfully updated!")
+
+date = open("assets/LastUpdatedOn.txt","w")
+now = datetime.now()
+DateAndTime = now.strftime("%d/%m/%Y %H:%M:%S")
+date.write(DateAndTime)
+date.close()
 
 input("Press any key to exit ")
