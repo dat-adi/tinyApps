acf46ae07063a96c79c783fe0d1b2f6810b95d65
dat-adi <naruita201@gmail.com>
2020-06-23 23:57:37 +0530

    Updated ytsearch with python script headers

--- YTsearch/ytsearch.py
+++ YTsearch/ytsearch.py
@@ -1,16 +1,32 @@
-import webbrowser as webb
-import argparse
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
-print("ytsearch 1.0.0 | tinyApps")
+# Web browser deployment
+from webbrowser import open as wbopen
 
-ap = argparse.ArgumentParser()
-ap.add_argument('-s', '--search', help="Enter the search request") #takes the arguments 
-args = vars(ap.parse_args()) #creates a dictionary for args to take the search request
+# Argument parsing
+from argparse import ArgumentParser
 
-if not args.get("search", False):
-    search_query = input("SearchQuery> ")
-else:
-    search_query = args["search"]
+'''YTSearch is a simple searching tool which retrieves results from YouTube and presents them to you in a new web browser.'''
+# Owned
+__author__ = "Datta Adithya"
+__credits__ = ["Datta Adithya"]
+__license__ = "MIT"
+__maintainer__ = "Datta Adithya"
+__email__ = "dat.adithya@gmail.com"
 
-search_query = search_query.replace(" ", "+") # replaces the spaces in the url
-webb.open("https://www.youtube.com/results?search_query={}".format(search_query)) #searches for it in youtube
+# Main
+if __name__ == "__main__":
+    print("ytsearch 1.0.0 | tinyApps")
+
+    ap = ArgumentParser()
+    ap.add_argument('-s', '--search', help="Enter the search request") #takes the arguments 
+    args = vars(ap.parse_args()) #creates a dictionary for args to take the search request
+
+    if not args.get("search", False):
+        search_query = input("SearchQuery> ")
+    else:
+        search_query = args["search"]
+
+    search_query = search_query.replace(" ", "+") # replaces the spaces in the url
+    wbopen("https://www.youtube.com/results?search_query={}".format(search_query)) #searches for it in youtube
