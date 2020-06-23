83cac672931d16f14e5f3142cd28128aad673c91
dat-adi <naruita201@gmail.com>
2020-06-23 23:40:46 +0530

    finished up on the other aspects of documentation

--- oneSpotApp/oneSpotApp.py
+++ oneSpotApp/oneSpotApp.py
@@ -9,14 +9,16 @@ from tkinter import filedialog, Text
 import os
 import pickle
 
-apps = []
+"""oneSpotApp.py is a simple one click service that allows you to deploy all the applications that you wish to, through a one time setup process."""
 
-if os.path.isfile('save.txt'):
-    with open('save.txt', 'r') as f:
-        tempApps = f.read()
-        tempApps = tempApps.split(',')
-        apps = [x for x in tempApps if x.strip()]
+# Owned
+__author__ = "Datta Adithya"
+__credits__ = ["Datta Adithya"]
+__license__ = "MIT"
+__maintainer__ = "Datta Adithya"
+__email__ = "dat.adithya@gmail.com"
 
+# function to add the apps into the file
 def addApps():
     for widget in frame.winfo_children():
         widget.destroy()
@@ -30,30 +32,41 @@ def addApps():
         label = tk.Label(frame, text=app, bg="gray")
         label.pack()
 
+# function to deploy all the apps through the file locations provided.
 def runApps():
     for app in apps:
         os.startfile(app)
 
-root = tk.Tk()
+# main
+if __name__ == "__main__":
+    apps = []
 
-canvas = tk.Canvas(root, height=700, width=700, bg="#DC143C")
-canvas.pack()
+    if os.path.isfile('save.txt'):
+        with open('save.txt', 'r') as f:
+            tempApps = f.read()
+            tempApps = tempApps.split(',')
+            apps = [x for x in tempApps if x.strip()]
 
-frame = tk.Frame(root, bg="white")
-frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
+    root = tk.Tk()
 
-openFile = tk.Button(root, text="Open File", padx=10, pady=5, fg="white", bg="#DC143C", command=addApps)
-openFile.pack()
+    canvas = tk.Canvas(root, height=700, width=700, bg="#DC143C")
+    canvas.pack()
 
-runApps = tk.Button(root, text="Run Apps", padx=10, pady=5, fg="white", bg="#DC143C", command=runApps)
-runApps.pack()
+    frame = tk.Frame(root, bg="white")
+    frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
 
-for app in apps:
-    label = tk.Label(frame, text=app)
-    label.pack()
+    openFile = tk.Button(root, text="Open File", padx=10, pady=5, fg="white", bg="#DC143C", command=addApps)
+    openFile.pack()
 
-root.mainloop()
+    runApps = tk.Button(root, text="Run Apps", padx=10, pady=5, fg="white", bg="#DC143C", command=runApps)
+    runApps.pack()
 
-with open('save.txt', 'w') as f:
     for app in apps:
-        f.write(app + ',')
+        label = tk.Label(frame, text=app)
+        label.pack()
+
+    root.mainloop()
+
+    with open('save.txt', 'w') as f:
+        for app in apps:
+            f.write(app + ',')
