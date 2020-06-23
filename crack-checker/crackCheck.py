2a9d27fef83dadec4ff033cae51619f5d66257ef
dat-adi <naruita201@gmail.com>
2020-06-24 00:11:27 +0530

    Updated update.py to add in script headers.

--- crack-checker/crackCheck.py
+++ crack-checker/crackCheck.py
@@ -1,3 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+'''crackCheck is a application that is dedicated to checking whether or not the game is available cracked in real time, so long as you updated it.'''
+
+# Owned
+__author__ = "Datta Adithya"
+__credits__ = ["Datta Adithya"]
+__license__ = "MIT"
+__maintainer__ = "Datta Adithya"
+__email__ = "dat.adithya@gmail.com"
+
 def Crack_checker(title):
     file = open("./assets/Data.txt","r")
     data = file.read()
@@ -6,10 +18,12 @@ def Crack_checker(title):
     else:
         return False
 
-game = input(str("Enter the name of the game you want to check for : "))
-if(Crack_checker(game)):
-    print("Game is Cracked")
-else:
-    print("Game is not Cracked")
+if __name__ == "__main__":
+    game = input(str("Enter the name of the game you want to check for : "))
 
-input("Press any key to exit ")
+    if(Crack_checker(game)):
+        print("Game is Cracked")
+    else:
+        print("Game is not Cracked")
+
+    input("Press any key to exit ")
