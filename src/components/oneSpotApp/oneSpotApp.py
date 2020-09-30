1ea3dc16b67ef44230b8ed803325df7ce2fa3b58
dat-adi <dat.adithya@gmail.com>
2020-09-30 12:31:59 +0530

    done with the initial class based oneSpotApp.py

--- src/components/oneSpotApp/oneSpotApp.py
+++ src/components/oneSpotApp/oneSpotApp.py
@@ -3,13 +3,14 @@
 
 # Front end window
 import tkinter as tk
-from tkinter import filedialog
+from tkinter import filedialog, Text
 
 # System and File access
 import os
+import pickle
 
-"""oneSpotApp.py is a simple one click service that allows you to deploy all the applications that you wish to, 
-through a one time setup process. """
+"""oneSpotApp.py is a simple one click service that allows you to deploy all the applications that you wish to,
+ through a one time setup process."""
 
 # Owned
 __author__ = "Datta Adithya"
@@ -19,57 +20,59 @@ __maintainer__ = "Datta Adithya"
 __email__ = "dat.adithya@gmail.com"
 
 
-# function to add the apps into the file
-def addApps():
-    for widget in frame.winfo_children():
-        widget.destroy()
-
-    filename = filedialog.askopenfilename(initialdir="/", title="Select App to add",
-                                          filetypes=(('.exe files', "*.exe"), ("All files", "*.*"))
-                                          )
-    apps.append(filename)
-    print(filename)
-    for element in apps:
-        temp_label = tk.Label(frame, text=element, bg="gray")
-        temp_label.pack()
-
-
-# function to deploy all the apps through the file locations provided.
-def runApps():
-    for element in apps:
-        os.startfile(element)
-
-
-# main
-if __name__ == "__main__":
-    apps = []
-
+def apps_in_file():
     if os.path.isfile('save.txt'):
         with open('save.txt', 'r') as f:
             tempApps = f.read()
             tempApps = tempApps.split(',')
             apps = [x for x in tempApps if x.strip()]
+    return apps
 
-    root = tk.Tk()
 
-    canvas = tk.Canvas(root, height=700, width=700, bg="#DC143C")
-    canvas.pack()
+class mainApp(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        self.wm_title("One Spot App")
 
-    frame = tk.Frame(root, bg="white")
-    frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
+        # returns the app locations present in the file
+        self.apps = apps_in_file()
+        
+        canvas = tk.Frame(self, height=700, width=700, bg="#DC143C")
+        canvas.pack(side="top", fill="both", expand=True)
 
-    openFile = tk.Button(root, text="Open File", padx=10, pady=5, fg="white", bg="#DC143C", command=addApps)
-    openFile.pack()
+        frame = tk.Frame(self, bg="white")
+        frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
 
-    runApps = tk.Button(root, text="Run Apps", padx=10, pady=5, fg="white", bg="#DC143C", command=runApps)
-    runApps.pack()
+        openFile = tk.Button(self, text="Open File",
+                             padx=10, pady=5, fg="white", bg="#DC143C",
+                             command=self.add_apps)
+        openFile.pack()
 
-    for app in apps:
-        label = tk.Label(frame, text=app)
-        label.pack()
+        runApps = tk.Button(self, text="Run Apps",
+                            padx=10, pady=5, fg="white", bg="#DC143C",
+                            command=self.run_apps)
+        runApps.pack()
 
-    root.mainloop()
+        for app in self.apps:
+            label = tk.Label(frame, text=app)
+            label.pack()
 
-    with open('save.txt', 'w') as f:
-        for app in apps:
-            f.write(app + ',')
+    def add_apps(self):
+        filename = filedialog.askopenfilename(initialdir='/', title="Select App to add", filetypes=(('.exe files', "*.exe"), ("All files", "*.*")))
+        self.apps.append(filename)
+
+    def run_apps(self):
+        for element in self.apps:
+            os.startfile(element)
+
+    def write_to_file(self):
+        with open('save.txt', 'a') as f:
+            for element in self.apps:
+                f.write(element + ',')
+
+
+if __name__ == "__main__":
+    App = mainApp()
+    App.mainloop()
+    App.write_to_file()
+
