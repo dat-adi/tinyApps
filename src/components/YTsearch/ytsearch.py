75bfeb19b485ddc1a02717117efc0e4994d5dc94
dat-adi <dat.adithya@gmail.com>
2020-10-08 15:24:32 +0530

    flake8 corrections and pylint 7.73 rating

--- src/components/YTsearch/ytsearch.py
+++ src/components/YTsearch/ytsearch.py
@@ -7,7 +7,9 @@ from webbrowser import open as wbopen
 # Argument parsing
 from argparse import ArgumentParser
 
-'''YTSearch is a simple searching tool which retrieves results from YouTube and presents them to you in a new web browser.'''
+# YTSearch is a simple searching tool which retrieves
+# results from YouTube and presents them to you in a new web browser.
+
 # Owned
 __author__ = "Datta Adithya"
 __credits__ = ["Datta Adithya"]
@@ -16,26 +18,31 @@ __maintainer__ = "Datta Adithya"
 __email__ = "dat.adithya@gmail.com"
 
 
-class yts:
+class Yts:
     def __init__(self):
-        ap = ArgumentParser() 
-        # ap.add_arguments take the arguments 
+        ap = ArgumentParser()
+        # ap.add_arguments take the arguments
         ap.add_argument('-s', '--search', help="Enter the search request")
-        args = vars(ap.parse_args()) #creates a dictionary for args to take the search request
+        # vars creates a dictionary for args to take the search request in.
+        args = vars(ap.parse_args())
 
         if not args.get("search", False):
             self.search_query = input("SearchQuery> ")
         else:
             self.search_query = args["search"]
 
+    # run_search runs the search for the query on the browser
     def run_search(self):
-        self.search_query = self.search_query.replace(" ", "+") # replaces the spaces in the url
-        wbopen("https://www.youtube.com/results?search_query={}".format(self.search_query)) #searches for it in youtube
+        self.search_query = self.search_query.replace(" ", "+")
+        # performs a search for the query on youtube
+        wbopen(
+                "https://www.youtube.com/results?search_query={}"
+                .format(self.search_query)
+                )
 
 
 # Main
 if __name__ == "__main__":
-    print("ytsearch 1.0.0 | tinyApps")
-    test = yts()
+    print("Ytsearch 1.0.0 | tinyApps")
+    test = Yts()
     test.run_search()
-
