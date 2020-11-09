c171bea95fb17fb43dcd5a61a0827282354a5c35
dat-adi <dat.adithya@gmail.com>
2020-11-09 23:21:13 +0530

    Removed lines that are for user.

--- src/components/crawlers/components.py
+++ src/components/crawlers/components.py
@@ -135,6 +135,5 @@ def generate(html_files, novelname, author):
     )
     epub.close()
 
-# commented code below as the files do not need to be deleted, in dev mode.
 #    for x in html_files:
 #        os.remove(x)
